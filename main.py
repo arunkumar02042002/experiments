ca98ef7443bb7ff7a1a2f2091cfd08bb69202989
Arun Kumar <arun.kumar.2403gg@gmail.com>
2024-04-06 12:21:49 +0530

    I'm done

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class GenerateChartUtiity:
         image_base64 = base64.b64encode(buffer.read()).decode('utf-8')
         return image_base64
 
-        @staticmethod
+    @staticmethod
     def get_plot_object(categories: list, values: list):
         '''
         function to create plot images
@@ -57,31 +57,42 @@ class GenerateChartUtiity:
 
         return plt
 
-        @staticmethod
-        def get_base64_of_plt_object():
+    @staticmethod
+    def make_chart(categories: list, values: list):
+        plt = GenerateChartUtiity.get_plot_object(categories, values)
+        image_base64 = GenerateChartUtiity.convert_image_to_base64(plt)
+        return image_base64
 
-            # def create_planning_emotion_chart(planning: float = 0, emotion: float = 0):
-            #     pass
-            #     # Data
-            #     # categories = ['Planning', 'Emotion']
-            #     # values = [planning, emotion]
+        # def create_planning_emotion_chart(planning: float = 0, emotion: float = 0):
+        #     pass
+        #     # Data
+        #     # categories = ['Planning', 'Emotion']
+        #     # values = [planning, emotion]
 
-            #     # plt.figure(figsize=(6, 3))
-            #     # bars = plt.bar(categories, values, color='orange', edgecolor='black')
+        #     # plt.figure(figsize=(6, 3))
+        #     # bars = plt.bar(categories, values, color='orange', edgecolor='black')
 
-            #     # # Remove top and right spines
-            #     # plt.gca().spines['top'].set_visible(False)
-            #     # plt.gca().spines['right'].set_visible(False)
+        #     # # Remove top and right spines
+        #     # plt.gca().spines['top'].set_visible(False)
+        #     # plt.gca().spines['right'].set_visible(False)
 
-            #     @staticmethod
-            #     def create_interpersonal_chart(values: list = list):
-            #         # Sample data
-            #         categories = ['Resoning', 'Numeric', 'Geometry']
-            #         values = [33.7247393072851, 44.351858926981, 28.259835915192]
+        #     @staticmethod
+        #     def create_interpersonal_chart(values: list = list):
+        #         # Sample data
+        #         categories = ['Resoning', 'Numeric', 'Geometry']
+        #         values = [33.7247393072851, 44.351858926981, 28.259835915192]
 
-            #         # get the base64 string
-            #         image_base64 = convert_image_to_base64(plt)
-            #         return image_base64
+        #         # get the base64 string
+        #         image_base64 = convert_image_to_base64(plt)
+        #         return image_base64
 
-            # image_base64 = create_interpersonal_chart()
-            # print(image_base64)
+        # image_base64 = create_interpersonal_chart()
+        # print(image_base64)
+
+
+if __name__ == '__main__':
+    # categories = ['Resoning2', 'Numeric', 'Geometry']
+    # values = [53.7247393072851, 44.351858926981, 25.259835915192]
+    # images_base64 = GenerateChartUtiity.make_chart(categories, values)
+    # print(images_base64)
+    pdfkit.from_file('invoice.html', 'index.pdf')
